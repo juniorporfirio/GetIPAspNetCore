0b07604913a04173fac6269c5b8389fa80df9dda
CLAUDEMIR PORFIRIO JUNIOR <Junior_Porfirio@hotmail.com>
2018-05-10 14:21:24 -0300

    Implementação de Demo para obter um endereço IP em ASP.NET Core

---
+++ GetIPAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GetIPAspNetCore.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace GetIPAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor contexto;
+
+        public HomeController(IHttpContextAccessor contexto)
+        {
+            this.contexto = contexto;
+        }
+        public IActionResult Index()
+        {
+            var ip = this.contexto.HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewBag.IP = ip;
+
+            return View();
+        }
+
+        public IActionResult About([FromServices]IHttpContextAccessor context)
+        {
+            var ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewBag.IP = ip;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+           
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
